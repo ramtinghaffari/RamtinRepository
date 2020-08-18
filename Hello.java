ef9e816219ead24dced2158be535c81f0e57261b
ramtinghaffari <31971373+ramtinghaffari@users.noreply.github.com>
2020-08-18 11:37:59 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*
+ * Hello World
+ * Ramtin Ghaffari
+ * 8/18/2020
+ * CMSC 255 003
+*/
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		System.out.println("<Your Name>");
+		System.out.println("<Java Project Name>");
+		System.out.println("<Project Description>");
+		System.out.println("<Version Date>");
+		System.out.println("<Course Number and Section>");
+		System.out.println();
+	}
+}
