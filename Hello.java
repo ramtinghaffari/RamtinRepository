055fbf02f9037114c2e694eb79f015df93412165
ramtinghaffari <31971373+ramtinghaffari@users.noreply.github.com>
2020-08-18 12:17:04 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*
+ * Hello World
+ * Ramtin Ghaffari
+ * 8/18/2020
+ * CMSC 255 003
+*/
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Ramtin Ghaffari");
+		System.out.println("Hello.java");
+		System.out.println("The goal of the lab is to first Java and all related applications and make sure that you can run your Java program using the system.out.println command.");
+		System.out.println("8/18/2020");
+		System.out.println("CMSC 255 003");
+		System.out.println();
+	}
+}
